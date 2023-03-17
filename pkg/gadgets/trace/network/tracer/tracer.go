CyberFlameGO/inspektor-gadget

commit 9a0941298c639e4a52d1d98b9c69b1cb603f113a

update gadgets to use new interfaces
This adds the required implementations for GadgetDesc and Gadget to
all gadgets. This commit tries to change as little as possible in the
existing gadget code to keep backwards compatibility at the price of
being a bit verbose or using not 100% fitting interfaces. We certainly
need to clean this up once the architecture has been implemented for
kubectl-gadget and gadgettracermgr as well.

pkg/gadgets/trace/network/tracer/tracer.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2022 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//go:build !withoutebpf
 
 package tracer
 
@@ -26,6 +28,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
 	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/network/types"
@@ -68,6 +71,9 @@ type Tracer struct {
 	// containers.
 	sync.Mutex
 	cache []*types.Event
+
+	eventCallback func(ev *types.Event)
+	gadgetCtx     gadgets.GadgetContext
 }
 
 func NewTracer(enricher gadgets.DataEnricherByNetNs) (_ *Tracer, err error) {
@@ -221,14 +227,17 @@ func (t *Tracer) Pop() (events []*types.Event, err error) {
 				Type:      eventtypes.NORMAL,
 				Timestamp: gadgets.WallTimeFromBootTime(val),
 			},
-			PktType:    pktTypeString(int(key.PktType)),
-			Proto:      protoString(int(key.Proto)),
-			Port:       gadgets.Htons(key.Port),
-			RemoteAddr: ip.String(),
+			PktType:     pktTypeString(int(key.PktType)),
+			Proto:       protoString(int(key.Proto)),
+			Port:        gadgets.Htons(key.Port),
+			RemoteAddr:  ip.String(),
+			WithNetNsID: eventtypes.WithNetNsID{NetNsID: key.ContainerNetns},
 		}
 
 		if t.enricher != nil {
 			t.enricher.EnrichByNetNs(&e.CommonData, key.ContainerNetns)
+		} else {
+			t.eventCallback(e)
 		}
 		return e
 	}
@@ -332,4 +341,61 @@ func (t *Tracer) Close() {
 		t.releaseAttachment(key, l)
 	}
 	t.networkGraphMapObjects.Close()
+}
+
+// --- Registry changes
+// TODO: This can be optimized a lot after using NewInstance() for everything
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	tracer := &Tracer{
+		attachments: make(map[uint64]*attachment),
+	}
+	return tracer, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	// Load the eBPF map
+	t.gadgetCtx = gadgetCtx
+	specMap, err := loadGraphmap()
+	if err != nil {
+		return fmt.Errorf("failed to load asset: %w", err)
+	}
+	if err := specMap.LoadAndAssign(&t.networkGraphMapObjects, &ebpf.CollectionOptions{}); err != nil {
+		return fmt.Errorf("failed to load ebpf program: %w", err)
+	}
+	return nil
+}
+
+func (t *Tracer) Start() error {
+	ctx := t.gadgetCtx.Context()
+	for {
+		if ctx.Err() != nil {
+			return nil
+		}
+		// Pop, but we don't need the results as we're handing the events over to the callback in cbMap
+		_, err := t.Pop()
+		if err != nil {
+			t.gadgetCtx.Logger().Debugf("pop() returned with: %v", err)
+			return nil
+		}
+	}
+}
+
+func (t *Tracer) Stop() {
+}
+
+func (t *Tracer) SetEventHandler(handler any) {
+	nh, ok := handler.(func(ev *types.Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventCallback = nh
+}
+
+func (t *Tracer) AttachContainer(container *containercollection.Container) error {
+	return t.Attach(container.Pid)
+}
+
+func (t *Tracer) DetachContainer(container *containercollection.Container) error {
+	return t.Detach(container.Pid)
 }

pkg/gadgets/advise/seccomp/tracer/tracer.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !withoutebpf
+
 package tracer
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
-	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	libseccomp "github.com/seccomp/libseccomp-golang"
+
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET -cc clang seccomp ./bpf/seccomp.bpf.c -- -I./bpf/ -I../../../../${TARGET}
@@ -41,6 +46,10 @@ type Tracer struct {
 	// the garbage collector might unlink it via the finalizer at any
 	// moment.
 	progLink link.Link
+
+	// We keep references to mountns of containers we attach to, so we
+	// can collect information afterwards
+	containers map[*containercollection.Container][]string
 }
 
 func NewTracer() (*Tracer, error) {
@@ -118,4 +127,51 @@ func (t *Tracer) Delete(mntns uint64) {
 func (t *Tracer) Close() {
 	t.progLink = gadgets.CloseLink(t.progLink)
 	t.objs.Close()
+}
+
+// ---
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	t := &Tracer{
+		containers: make(map[*containercollection.Container][]string),
+	}
+	return t, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	return nil
+}
+
+func (t *Tracer) Stop() {
+}
+
+func (t *Tracer) Start() error {
+	if err := t.start(); err != nil {
+		t.Stop()
+		return err
+	}
+	return nil
+}
+
+func (t *Tracer) AttachContainer(container *containercollection.Container) error {
+	t.containers[container] = nil
+	return nil
+}
+
+func (t *Tracer) DetachContainer(container *containercollection.Container) error {
+	res, err := t.Peek(container.Mntns)
+	if err != nil {
+		t.containers[container] = []string{err.Error()}
+		return nil
+	}
+	t.containers[container] = res
+	return nil
+}
+
+func (t *Tracer) Result() ([]byte, error) {
+	out := make(map[string][]string)
+	for container, result := range t.containers {
+		out[container.Name] = result
+	}
+	return json.MarshalIndent(out, "", "  ")
 }

pkg/gadgets/audit/seccomp/tracer/tracer.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2022 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !withoutebpf
+
 package tracer
 
 import (
@@ -23,6 +25,7 @@ import (
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/perf"
+
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/audit/seccomp/types"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
@@ -140,11 +143,11 @@ func (t *Tracer) run() {
 				Type:      eventtypes.NORMAL,
 				Timestamp: gadgets.WallTimeFromBootTime(eventC.Timestamp),
 			},
-			Pid:       uint32(eventC.Pid),
-			MountNsID: uint64(eventC.MntnsId),
-			Syscall:   syscallToName(int(eventC.Syscall)),
-			Code:      codeToName(uint(eventC.Code)),
-			Comm:      gadgets.FromCString(eventC.Comm[:]),
+			Pid:           uint32(eventC.Pid),
+			WithMountNsID: eventtypes.WithMountNsID{MountNsID: eventC.MntnsId},
+			Syscall:       syscallToName(int(eventC.Syscall)),
+			Code:          codeToName(uint(eventC.Code)),
+			Comm:          gadgets.FromCString(eventC.Comm[:]),
 		}
 
 		if t.enricher != nil {
@@ -161,4 +164,40 @@ func (t *Tracer) Close() {
 		t.reader.Close()
 	}
 	t.objs.Close()
+}
+
+// ---
+
+func (t *Tracer) Start() error {
+	if err := t.start(); err != nil {
+		t.Stop()
+		return err
+	}
+	return nil
+}
+
+func (t *Tracer) SetMountNsMap(mountnsMap *ebpf.Map) {
+	t.config.MountnsMap = mountnsMap
+}
+
+func (t *Tracer) SetEventHandler(handler any) {
+	nh, ok := handler.(func(ev *types.Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventCallback = nh
+}
+
+func (t *Tracer) Stop() {
+}
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	t := &Tracer{
+		config: &Config{},
+	}
+	return t, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	return nil
 }

pkg/gadgets/internal/networktracer/tracer.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Inspektor Gadget authors
+// Copyright 2022-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
 	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/internal/socketenricher"
@@ -137,6 +138,8 @@ type Tracer[Event any] struct {
 
 	baseEvent    func(ev types.Event) *Event
 	processEvent func(rawSample []byte, netns uint64) (*Event, error)
+
+	eventHandler func(ev *Event)
 }
 
 func NewTracer[Event any](
@@ -196,6 +199,22 @@ func (t *Tracer[Event]) Attach(pid uint32, eventCallback func(*Event)) error {
 	go t.listen(netns, a.perfRd, t.baseEvent, t.processEvent, eventCallback)
 
 	return nil
+}
+
+func (t *Tracer[Event]) SetEventHandler(handler any) {
+	nh, ok := handler.(func(ev *Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventHandler = nh
+}
+
+func (t *Tracer[Event]) AttachContainer(container *containercollection.Container) error {
+	return t.Attach(container.Pid, t.eventHandler)
+}
+
+func (t *Tracer[Event]) DetachContainer(container *containercollection.Container) error {
+	return t.Detach(container.Pid)
 }
 
 func (t *Tracer[Event]) listen(

pkg/gadgets/profile/block-io/tracer/tracer.go
@@ -1,7 +1,4 @@
-//go:build linux
-// +build linux
-
-// Copyright 2019-2021 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//go:build !withoutebpf
 
 package tracer
 
@@ -38,6 +37,8 @@ type Tracer struct {
 	blockRqCompleteLink link.Link
 	blockRqInsertLink   link.Link
 	blockRqIssueLink    link.Link
+	result              string
+	err                 error
 }
 
 func NewTracer() (*Tracer, error) {
@@ -148,4 +149,31 @@ func (t *Tracer) start() error {
 	t.blockRqIssueLink = blockRqIssueLink
 
 	return nil
+}
+
+// ---
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	t := &Tracer{}
+	return t, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	return nil
+}
+
+func (t *Tracer) StartAlt() error {
+	if err := t.start(); err != nil {
+		t.Stop()
+		return err
+	}
+	return nil
+}
+
+func (t *Tracer) StopAlt() {
+	t.result, t.err = t.Stop()
+}
+
+func (t *Tracer) Result() ([]byte, error) {
+	return []byte(t.result), t.err
 }

pkg/gadgets/profile/cpu/tracer/tracer.go
@@ -1,7 +1,4 @@
-//go:build linux
-// +build linux
-
-// Copyright 2019-2021 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//go:build !withoutebpf
 
 package tracer
 
@@ -35,6 +34,7 @@ import (
 
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/profile/cpu/types"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target $TARGET -type key_t -cc clang profile ./bpf/profile.bpf.c -- -I./bpf/ -I../../../../${TARGET}
@@ -380,5 +380,73 @@ func (t *Tracer) start() error {
 		}
 	}
 
+	return nil
+}
+
+// ---
+
+// TracerWrap is required to implement interfaces
+type TracerWrap struct {
+	Tracer
+	enricherFunc  func(ev any) error
+	eventCallback func(ev *types.Report)
+}
+
+func (t *TracerWrap) Start() error {
+	if err := t.start(); err != nil {
+		t.Stop()
+		return err
+	}
+	return nil
+}
+
+func (t *TracerWrap) Stop() {
+	// TODO: Error handling on stop?
+	res, _ := t.Tracer.Stop()
+	var reports []*types.Report
+	json.Unmarshal([]byte(res), &reports)
+	for _, report := range reports {
+		t.eventCallback(report)
+	}
+}
+
+func (t *TracerWrap) SetEventHandler(handler any) {
+	nh, ok := handler.(func(ev *types.Report))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventCallback = nh
+}
+
+func (t *TracerWrap) SetEventEnricher(enricher func(ev any) error) {
+	t.enricherFunc = enricher
+	t.Tracer.enricher = t
+}
+
+func (t *TracerWrap) EnrichByMntNs(event *eventtypes.CommonData, mountnsid uint64) {
+	// TODO: This is ugly as it temporarily wraps and unwraps the event; should be changed in the original gadget code
+	//  after full migration to NewInstance()
+	wrap := &types.Report{CommonData: *event, MntnsID: mountnsid}
+	t.enricherFunc(wrap)
+	*event = wrap.CommonData
+}
+
+func (t *TracerWrap) SetMountNsMap(mountNsMap *ebpf.Map) {
+	t.Tracer.config.MountnsMap = mountNsMap
+}
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	tracer := &TracerWrap{
+		Tracer: Tracer{
+			config: &Config{},
+		},
+	}
+	return tracer, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	params := gadgetCtx.GadgetParams()
+	t.config.UserStackOnly = params.Get(ParamUserStack).AsBool()
+	t.config.KernelStackOnly = params.Get(ParamKernelStack).AsBool()
 	return nil
 }

pkg/gadgets/snapshot/process/tracer/tracer.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 The Inspektor Gadget authors
+// Copyright 2019-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//go:build !withoutebpf
 
 package tracer
 
@@ -136,11 +138,11 @@ func runeBPFCollector(config *Config, enricher gadgets.DataEnricherByMntNs) ([]*
 			Event: eventtypes.Event{
 				Type: eventtypes.NORMAL,
 			},
-			Pid:       tgid,
-			Tid:       pid,
-			Command:   command,
-			ParentPid: parentPid,
-			MountNsID: mntnsid,
+			Pid:           tgid,
+			Tid:           pid,
+			Command:       command,
+			ParentPid:     parentPid,
+			WithMountNsID: eventtypes.WithMountNsID{MountNsID: mntnsid},
 		}
 
 		if enricher != nil {
@@ -176,14 +178,14 @@ func getTidEvent(config *Config, enricher gadgets.DataEnricherByMntNs, pid, tid 
 		Event: eventtypes.Event{
 			Type: eventtypes.NORMAL,
 		},
-		Tid:       tid,
-		Pid:       pid,
-		Command:   comm,
-		MountNsID: mntnsid,
+		Tid:           tid,
+		Pid:           pid,
+		Command:       comm,
+		WithMountNsID: eventtypes.WithMountNsID{MountNsID: mntnsid},
 	}
 
 	if enricher != nil {
-		enricher.EnrichByMntNs(&event.CommonData, event.MountNsID)
+		enricher.EnrichByMntNs(&event.CommonData, event.WithMountNsID.MountNsID)
 	}
 
 	return event, nil
@@ -253,4 +255,45 @@ func runProcfsCollector(config *Config, enricher gadgets.DataEnricherByMntNs) ([
 	}
 
 	return events, nil
+}
+
+// ---
+
+type Tracer struct {
+	config       *Config
+	eventHandler func(ev []*processcollectortypes.Event)
+}
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	tracer := &Tracer{
+		config: &Config{},
+	}
+	return tracer, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	params := gadgetCtx.GadgetParams()
+	t.config.ShowThreads = params.Get(ParamThreads).AsBool()
+	return nil
+}
+
+func (t *Tracer) SetEventHandlerArray(handler any) {
+	nh, ok := handler.(func(ev []*processcollectortypes.Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventHandler = nh
+}
+
+func (t *Tracer) SetMountNsMap(mntnsMap *ebpf.Map) {
+	t.config.MountnsMap = mntnsMap
+}
+
+func (t *Tracer) Run() error {
+	processes, err := RunCollector(t.config, nil)
+	if err != nil {
+		return err
+	}
+	t.eventHandler(processes)
+	return nil
 }

pkg/gadgets/snapshot/socket/tracer/tracer.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Inspektor Gadget authors
+// Copyright 2021-2023 The Inspektor Gadget authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,7 +10,9 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License
+// limitations under the License.
+
+//go:build !withoutebpf
 
 package tracer
 
@@ -22,6 +24,9 @@ import (
 
 	"github.com/cilium/ebpf/link"
 
+	containercollection "github.com/inspektor-gadget/inspektor-gadget/pkg/container-collection"
+	containerutils "github.com/inspektor-gadget/inspektor-gadget/pkg/container-utils"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	socketcollectortypes "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/snapshot/socket/types"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/netnsenter"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
@@ -161,9 +166,17 @@ func RunCollector(pid uint32, podname, namespace, node string, proto socketcolle
 					return err
 				}
 
+				// TODO: Receive the netns from caller
+				netns, err := containerutils.GetNetNs(int(pid))
+				if err != nil {
+					return fmt.Errorf("getting netns for pid %d: %w", pid, err)
+				}
+
 				sockets = append(sockets, &socketcollectortypes.Event{
 					Event: eventtypes.Event{
 						Type: eventtypes.NORMAL,
+						// TODO: This can be removed as events will be enriched
+						//  by the eventHandler
 						CommonData: eventtypes.CommonData{
 							Node:      node,
 							Namespace: namespace,
@@ -177,6 +190,7 @@ func RunCollector(pid uint32, podname, namespace, node string, proto socketcolle
 					RemotePort:    destp,
 					Status:        status,
 					InodeNumber:   inodeNumber,
+					WithNetNsID:   eventtypes.WithNetNsID{NetNsID: netns},
 				})
 			}
 
@@ -192,4 +206,67 @@ func RunCollector(pid uint32, podname, namespace, node string, proto socketcolle
 	}
 
 	return sockets, nil
+}
+
+// ---
+
+type Tracer struct {
+	// visitedNamespaces is a map where the key is the netns inode number and
+	// the value is the pid of one of the containers that share that netns. Such
+	// pid is used by NetnsEnter. TODO: Improve NetnsEnter to also work with the
+	// netns directly.
+	visitedNamespaces map[uint64]uint32
+	protocols         string
+	eventHandler      func([]*socketcollectortypes.Event)
+}
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	tracer := &Tracer{
+		visitedNamespaces: make(map[uint64]uint32),
+	}
+	return tracer, nil
+}
+
+func (t *Tracer) Init(gadgetCtx gadgets.GadgetContext) error {
+	params := gadgetCtx.GadgetParams()
+	t.protocols = params.Get(ParamProto).AsString()
+	return nil
+}
+
+func (t *Tracer) AttachContainer(container *containercollection.Container) error {
+	if _, ok := t.visitedNamespaces[container.Netns]; ok {
+		return nil
+	}
+	t.visitedNamespaces[container.Netns] = container.Pid
+	return nil
+}
+
+func (t *Tracer) DetachContainer(container *containercollection.Container) error {
+	return nil
+}
+
+func (t *Tracer) SetEventHandlerArray(handler any) {
+	nh, ok := handler.(func(ev []*socketcollectortypes.Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventHandler = nh
+}
+
+func (t *Tracer) Run() error {
+	allSockets := []*socketcollectortypes.Event{}
+
+	for netns, pid := range t.visitedNamespaces {
+		// TODO: Remove podname, namespace and node arguments from RunCollector.
+		// The enrichment will be done in the event handler. In addition, pass
+		// the netns to avoid retrieving it again in RunCollector.
+		sockets, err := RunCollector(pid, "", "", "", socketcollectortypes.ProtocolsMap[t.protocols])
+		if err != nil {
+			return fmt.Errorf("collecting sockets in netns %d: %w", netns, err)
+		}
+		allSockets = append(allSockets, sockets...)
+	}
+
+	t.eventHandler(allSockets)
+	return nil
 }

commit 590c0dfd73a495e64611055a234031e27252b30f

trace/network: Use ticker to query results
The trace network gadget is designed to give a summary of the network
connections performed by a pod / container. The eBPF program updates
a map that is then read by the user space part. This operation happens
each second, this is a tradeoff between reporting new connections fast
enough and not using too many CPU to poll the maps.

This mechanism was wrongly removed in
9a0941298c63 ("update gadgets to use new interfaces"), this commit
adds it again.

Fixes: 9a0941298c63 ("update gadgets to use new interfaces")

pkg/gadgets/trace/network/tracer/tracer.go
@@ -24,6 +24,7 @@ import (
 	"net"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/cilium/ebpf"
 	log "github.com/sirupsen/logrus"
@@ -385,16 +386,21 @@ func (t *Tracer) install() error {
 }
 
 func (t *Tracer) Run(gadgetCtx gadgets.GadgetContext) error {
-	for {
-		if t.ctx.Err() != nil {
-			return nil
-		}
+	ctx := t.gadgetCtx.Context()
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 
-		// Pop, but we don't need the results as we're handing the events over
-		// to the callback in cbMap
-		_, err := t.Pop()
-		if err != nil {
-			return err
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-ticker.C:
+			// Pop, but we don't need the results as we're handing the events over to the callback
+			_, err := t.Pop()
+			if err != nil {
+				t.gadgetCtx.Logger().Debugf("pop() returned with: %v", err)
+				return nil
+			}
 		}
 	}
 }